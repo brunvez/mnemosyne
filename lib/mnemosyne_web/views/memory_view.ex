cfe899aa0d660625d91eda8b1e6c533c81fbe452
Bruno Vezoli <brunvezoli@hotmail.com>
2019-07-25 00:48:14 -0300

    Add markdown support to description

--- lib/mnemosyne_web/views/memory_view.ex
+++ lib/mnemosyne_web/views/memory_view.ex
@@ -1,17 +1,17 @@
 defmodule MnemosyneWeb.MemoryView do
   use MnemosyneWeb, :view
 
+  defp parse_description(nil), do: ""
+  defp parse_description(description) do
+    case Earmark.as_html(description) do
+      {:ok, html, _} -> html
+      _ -> gettext("Couldn't parse description")
+    end
+  end
+
   defp enumerate_tags(tags) do
     tags
     |> Enum.map(& &1.name)
     |> Enum.join(", ")
-  end
-
-  defp stringify_tags(%Ecto.Association.NotLoaded{} = _tags), do: ""
-
-  defp stringify_tags(tags) do
-    tags
-    |> Enum.map(& &1.name)
-    |> Enum.join(",")
   end
 end
